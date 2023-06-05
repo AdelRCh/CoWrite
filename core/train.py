e06ed2bd2832b392309d6d488c1766ba5e3339a7
Adel Chouadria <adelchouadria@gmail.com>
2023-06-05 17:02:11 +0200

    Several adjustments made to the project's file structure.

---
+++ core/train.py
@@ -0,0 +1,9 @@
+'''
+The routines which will help us train three components:
+- Grammar checking
+- Coreference identification
+- Summarizer
+
+If we get to that point, we will also have a function to train our stylebook
+checker depending on our stylebook du jour.
+'''
