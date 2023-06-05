d7554f6cfcfef5d1593d69cbce32d90243069a92
Adel Chouadria <adelchouadria@gmail.com>
2023-06-05 17:03:21 +0200

    Comment tweak

---
+++ core/paragraph.py
@@ -0,0 +1,10 @@
+'''
+Here are the methods used to view paragraphs in their entirety.
+We will be using a pretrained summarizing model to help with the task.
+Its outputs will help us judge which sentences should come first, and how
+we should reorder our texts depending on the situation.
+
+The MVP will examine the feasability of news-style inverted pyramid layouts.
+However, we acknowledge that documents of different natures would highlight
+different features.
+'''
