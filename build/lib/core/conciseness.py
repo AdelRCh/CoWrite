158f5335c1c55bded67249bfe3746d6b23f76847
Sarah Carr <sjcarr87@gmail.com>
2023-06-08 17:40:53 +0200

    Commit for updated method files

---
+++ build/lib/core/conciseness.py
@@ -0,0 +1,7 @@
+'''
+If we get to the point where we implement stylebooks and conciseness routines,
+these methods should help us figure things out.
+This .py file may become deprecated, depending on the size of our preprocess.py
+section. However, that is unlikely, as the methods used here are of a
+different nature.
+'''
--- core/preprocess.py
+++ core/preprocess.py
@@ -6,3 +6,37 @@ We will have our preprocessing routines grouped here for ease of use.
 - Coreference identifier: input processing
 - Summarizer: initializing the model, loading the data, and training it further
 '''
+import re
+import gc
+
+def process_input(full_text):
+    #Separate the paragraph
+    paragraphs = full_text.split('\n')
+    corrected_paragraphs = []
+    for para in paragraphs:
+        #a.candy.is.a.3.5 candy. => a. candy. is. a.3.5 candy.
+        #I want to find all instances of these periods and fix them.
+        search = re.findall(r'\.[a-zA-Z]', para)
+
+        for bad_dot in search:
+            location = para.find(bad_dot)
+            #I want to include the period in the sentence before splitting.
+            para = para[:location+1] + ' ' + para[location+1:]
+
+        #We will split each paragraph into sentences and correct each one.
+        split_sentences = para.split(sep='. ')
+        corrections = []
+        for sentence in split_sentences:
+            #happy_t5 is the model variable after loading. Must be renamed, I think.
+            #Alert: preventing an error when people do two line carries in a row ("one of the paragraphs is empty? What sorcery is this.")
+            #It's the equivalent of the "you KICK Miette?" meme tweet, except for the model.
+            if sentence: #Not a line carry
+                prelim_result = happy_t5.generate_text(sentence, args=beam_settings)
+                corrections.append(prelim_result.text.capitalize())
+            else:
+                corrections.append(sentence)
+
+        result = ' '.join(corrections)
+        corrected_paragraphs.append(result)
+
+    return '\n'.join(corrected_paragraphs)
